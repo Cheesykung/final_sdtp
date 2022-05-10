2159be6daf7f813d51eb09b95d4f26895dfe6b56
Cheese kung <fookkub1@gmail.com>
2022-05-10 09:35:39 +0700

    add docker file

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongo = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017";
+const url = "mongodb://localhost:20000";
+// const url = "mongodb://host.docker.internal:20000";
 
 app.use(express.json())
 app.use(cors("*"))
@@ -20,6 +21,9 @@ mongo.connect(
       console.error(err)
       return
     }
+    const dbo = client.db("test");
+    dbo.createCollection("test")
+
     db = client.db("test")
     test = db.collection("test")
   }
@@ -29,6 +33,24 @@ app.get('/', async (req, res) => {
   await test.insertOne({ name: "Thai" })
 
   res.status(200).send("eiei")
+})
+
+// app.get('/hello', async (req, res) => {
+//   await test.insertOne({ name: "Thai" })
+
+//   let result = await test.find().toArray()
+
+//   res.status(200).send(result)
+// })
+
+app.get('/:name', async (req, res) => {
+  let name = req.params["name"]
+
+  let result = await test.find({
+    "name": name
+  }).toArray()
+
+  res.status(200).send(result)
 })
 
 app.listen(3000)
